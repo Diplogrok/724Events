196fa43e1cf5dfa670af8c60977aab57697aefff
Alexia Stephan <alexia0stephan@gmail.com>
2024-02-23 15:32:17 +0100

    correction code filtrage des evenement test succed

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -7,34 +7,46 @@ import ModalEvent from "../ModalEvent";
 
 import "./style.css";
 
-const PER_PAGE = 9;
-
+const PER_PAGE = 9; // Nombre d'événements par page
 const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
-  const changeType = (evtType) => {
+
+  // Fonction pour changer le type de catégorie sélectionné et revenir à la première page
+  const changeType = (eType) => {
     setCurrentPage(1);
-    setType(evtType);
+    setType(eType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+
+  let filteredEvents = []; // Variable initialisé avec un tableau vide
+  let totalPages = 0; // Variable initialisé à 0
+
+  // Filtrage des événements en fonction du type de catégorie sélectionné
+  if (!type) {
+    // Si aucun type n'est sélectionné, afficher tous les événements
+    const startIndex = (currentPage - 1) * PER_PAGE;
+    const endIndex = Math.min(startIndex + PER_PAGE, data?.events?.length || 0);
+    filteredEvents = data?.events?.slice(startIndex, endIndex) || [];
+    totalPages = Math.ceil((data?.events?.length || 0) / PER_PAGE);
+  } else {
+    // Sinon, filtrer les événements par type de catégorie
+    filteredEvents = data?.events?.filter((event) => event.type === type) || [];
+  }
+  // Tri des événements par date
+  filteredEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  // Liste des types de catégories disponibles
+  const typeList = new Set(data?.events?.map((event) => event.type) || []);
+
+  // Fonction pour gérer le changement de page
+  const handleClickPage = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
-      {error && <div>An error occured</div>}
+      {error && <div>An error occurred</div>}
       {data === null ? (
         "loading"
       ) : (
@@ -59,14 +71,24 @@ const EventList = () => {
               </Modal>
             ))}
           </div>
-          <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
-              </a>
-            ))}
-          </div>
+          {totalPages > 1 && ( // Vérifie s'il y a plus d'une page à afficher
+            <div className="Pagination">
+              {[...Array(totalPages)].map(
+                (
+                  _,
+                  index // Boucle à travers le nombre total de pages
+                ) => (
+                  <button
+                    key={`page-${index + 1}`} // Clé unique pour chaque bouton de pagination
+                    className={currentPage === index + 1 ? "active" : ""} // Applique la classe 'active' si le bouton correspond à la page actuelle
+                    type="button"
+                    onClick={() => handleClickPage(index + 1)}>
+                    {index + 1}
+                  </button>
+                )
+              )}
+            </div>
+          )}
         </>
       )}
     </>
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,33 +10,44 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
-
   const nextCard = () => {
-    setIndex((prevIndex) => (prevIndex + 1) % byDateDesc.length);
+    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
   };
-
   useEffect(() => {
-    const intervalId = setInterval(nextCard, 5000);
-    return () => clearInterval(intervalId);
-  }, []);
-
+    nextCard();
+  });
   return (
     <div className="SlideCardList">
+      {console.log("Contenu de byDateDesc:", byDateDesc)}
       {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.id}
-          className={`SlideCard SlideCard--${
-            index === idx ? "display" : "hide"
-          }`}>
-          <img src={event.cover} alt="forum" />
-          <div className="SlideCard__descriptionContainer">
-            <div className="SlideCard__description">
-              <h3>{event.title}</h3>
-              <p>{event.description}</p>
-              <div>{getMonth(new Date(event.date))}</div>
+        <>
+          <div
+            key={event.title}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}>
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
             </div>
           </div>
-        </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((_, radioIdx) => (
+                <input
+                  key={`${event.id}`}
+                  type="radio"
+                  name="radio-button"
+                  checked={idx === radioIdx}
+                />
+              ))}
+            </div>
+          </div>
+        </>
       ))}
     </div>
   );
